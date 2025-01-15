eb4ab5c5ace3ebac7f6db55ea386b91f48317ec5
MANOJ1804 <69451602+MANOJ1804@users.noreply.github.com>
2025-01-15 16:39:26 +0530

    Update backend.tf

--- EKS_Terraform/backend.tf
+++ EKS_Terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "manoj-aws1804" # Replace with your actual S3 bucket name
+    bucket = "manoj-aws-18045" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
